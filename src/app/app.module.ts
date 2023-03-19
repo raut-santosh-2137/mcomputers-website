401c8c45cde1f8027951a1d329e1d13630494049
santosh raut <santosh.raut@mobify.bz>
2023-03-19 14:02:59 +0530

    created pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { ProductComponent } from './pages/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesComponent,
+    ContactComponent,
+    AboutComponent,
+    ProductListComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
